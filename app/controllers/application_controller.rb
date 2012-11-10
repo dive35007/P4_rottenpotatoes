068aaa62226b5e23c26cc6d1f8d18e5565dd51d5
dive35007 <dive53007@gmail.com>
2012-11-10 22:59:47 +0100

    modified :except in Aplication_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_current_user, :except => 'login'
+  before_filter :except => ['login', :index, :show]
   protected # prevents method from being invoked by a route
   def set_current_user
     # we exploit the fact that find_by_id(nil) returns nil
     @current_user ||= Moviegoer.find_by_id(session[:user_id])
-    redirect_to '/login' and return unless @current_user
+    redirect_to '/login' # and return unless @current_user
   end
 end
