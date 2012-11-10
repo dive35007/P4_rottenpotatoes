7dbc4a69be43860bfa27f97f25b6a791681223f1
dive35007 <dive53007@gmail.com>
2012-11-11 00:22:25 +0100

    editing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   def set_current_user
     # we exploit the fact that find_by_id(nil) returns nil
     @current_user ||= Moviegoer.find_by_id(session[:user_id])
-    redirect_to '/login' and return unless => [@current_user, :index, :show, :similar, :search_tmdb]
+    redirect_to '/login' and return unless @current_user || :index || :show || :similar || :search_tmdb
   end
 end
