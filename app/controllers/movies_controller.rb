aa455434c3edaa1ff5a837b2881f09a706b71717
dive35007 <dive53007@gmail.com>
2012-11-11 04:50:09 +0100

    Ejercicio2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,8 @@
 class MoviesController < ApplicationController
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    id = params[:id]
+    @movie = Movie.find(id)
   end
 
   def index
@@ -37,13 +36,17 @@ class MoviesController < ApplicationController
   end
 
   def new
-    # default: render 'new' template
+    @movie = Movie.new(params[:movie])
   end
 
   def create
-    @movie = Movie.create!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    if params['commit'] != "Cancel"
+      @movie = Movie.create!(params[:movie])
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movie_path(@movie.id)
+    else
+      redirect_to movies_path
+    end
   end
 
   def edit
@@ -51,10 +54,12 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    redirect_to movie_path(@movie)
+    if params['commit'] != "Cancel"
+      @movie = Movie.find params[:id]
+      @movie.update_attributes!(params[:movie])
+      flash[:notice] = "#{@movie.title} was successfully updated."
+    end
+    redirect_to movie_path(params[:id])
   end
 
   def destroy
